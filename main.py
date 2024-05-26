3a9741d1b4ee6d4ccee17a52aff2738731308c6b
Klayton Pagel <pagelk@lopers.unk.edu>
2024-05-26 14:20:51 -0500

    Read Data
    Added a function to read the data from a file in S3

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ s3 = boto3.client(
 )
 
 
+# list objects in the bucket____________________________________________________________________________________________
+def list_objects(file_name: str) -> str:
+    objects = s3.get_object(Bucket=bucket, Key=file_name)
+    content = str(objects["Body"].read())[2:-1]
+    return content
+
+
 # upload file___________________________________________________________________________________________________________
 def upload():
     with open("hehe.txt", "rb") as f:
@@ -23,7 +30,9 @@ def upload():
 
 # Download file_________________________________________________________________________________________________________
 def download():
-    s3.download_file(bucket, "hehe.txt", "hehehe.txt")
+    s3.download_file(bucket, "Users/Employees/83GogrCqXcNd2bkDPEdOou8hyTtY0Qc2/dashboard.json", "hehehe.json")
 
 
-download()
+thing = eval(list_objects("Users/Employees/83GogrCqXcNd2bkDPEdOou8hyTtY0Qc2/dashboard.json"))
+
+print(thing['1'])
